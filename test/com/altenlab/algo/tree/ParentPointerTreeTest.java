d8e623fc21a065bd21b147d786670854559b7bc1
Alexander Tenishev <alextenishev@gmail.com>
2018-11-11 10:35:55 -0800

    add tests for par ptr tree

---
+++ test/com/altenlab/algo/tree/ParentPointerTreeTest.java
@@ -0,0 +1,33 @@
+package com.altenlab.algo.tree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParentPointerTreeTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void differ() {
+    }
+
+    @Test
+    void union() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void betterFind() {
+    }
+
+    @Test
+    void betterFindIterative() {
+    }
+
+}
