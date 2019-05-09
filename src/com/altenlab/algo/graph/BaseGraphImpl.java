c730d8bb820bf7334b63d90c134c35ce7d6d8bcb
Alexander Tenishev <alextenishev@gmail.com>
2019-05-08 18:49:02 -0700

    refactor graph implementation

--- src/com/altenlab/algo/graph/BaseGraphImpl.java
+++ src/com/altenlab/algo/graph/BaseGraphImpl.java
@@ -1,5 +1,99 @@
 package com.altenlab.algo.graph;
 
-public class BaseGraphImpl {
-    //TODO: make this class base of GraphList and GraphMatrix
+public abstract class BaseGraphImpl implements IGraph {
+    protected int numEdge; // Number of edges
+    public int[] Mark; // The mark array
+
+    public BaseGraphImpl(int n) {
+        Init(n);
+    }
+
+    public void Init(int n) {
+        Mark = new int[n];
+        numEdge = 0;
+    }
+
+    public int n() {
+        return Mark.length;
+    } // # of vertices
+
+    public int e() {
+        return numEdge;
+    } // # of edges
+
+    /** Set/Get the mark value for a vertex */
+    public void setMark(int v, int val) {
+        Mark[v] = val;
+    }
+    public int getMark(int v) {
+        return Mark[v];
+    }
+    public void resetAllMarks() {
+        for( int v = 0; v < n(); v++ ) {
+            setMark(v, VisitState.UNVISITED.ordinal());
+        }
+    }
+    @Override
+    public int compareTo(Object other) throws ClassCastException {
+        if( !(other instanceof IGraph) ) {
+            throw new ClassCastException("An IGraph object expected.");
+        }
+        final IGraph otherG = (IGraph) other;
+        if( this.Mark.length != otherG.n() ) {
+            return this.Mark.length < otherG.n() ? -1 : 1;
+        }
+        if( this.numEdge != otherG.e() ) {
+            return numEdge != otherG.e() ? -1 : 1;
+        }
+
+        this.resetAllMarks();
+
+        for (int v = 0; v < this.n(); v++) {
+            if( getMark(v) == VisitState.UNVISITED.ordinal() ) {
+                this.setMark(v, VisitState.VISITED.ordinal());
+                for( int w = this.first(v); w < this.n(); w = this.next(v, w)) {
+                    if( this.getMark(w) == VisitState.UNVISITED.ordinal() ) {
+                        if( this.weight(v, w) < otherG.weight(v,w) ) {
+                            return -1;
+                        } else if( this.weight(v, w) > otherG.weight(v,w) ) {
+                            return 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object other) throws ClassCastException {
+        if( !(other instanceof IGraph) ) {
+            throw new ClassCastException("An IGraph object expected.");
+        }
+        final IGraph otherG = (IGraph) other;
+        if( this.Mark.length != otherG.n() ) {
+            return false;
+        }
+        if( this.numEdge != otherG.e() ) {
+            return false;
+        }
+
+        this.resetAllMarks();
+
+        for (int v = 0; v < this.n(); v++) {
+            if( getMark(v) == VisitState.UNVISITED.ordinal() ) {
+                this.setMark(v, VisitState.VISITED.ordinal());
+                for( int w = this.first(v); w < this.n(); w = this.next(v, w)) {
+                    if( this.getMark(w) == VisitState.UNVISITED.ordinal() ) {
+                        if( this.weight(v, w) != otherG.weight(v,w) ) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 }
--- src/com/altenlab/algo/graph/GraphList.java
+++ src/com/altenlab/algo/graph/GraphList.java
@@ -1,26 +1,19 @@
 package com.altenlab.algo.graph;
 
-public class GraphList implements IGraph {
+public class GraphList extends BaseGraphImpl {
     private GraphListData[] vertex; // The vertex list
-    private int numEdge; // Number of edges
-    public int[] Mark; // The mark array
 
-    public GraphList() {
-    }
     public GraphList(int n) {
+        super(n);
         Init(n);
     }
     public void Init(int n) {
-        Mark = new int[n];
+        super.Init(n);
         vertex = new GraphListData[n];
         for( int i = 0; i < n; i++ ) {
             vertex[i] = new GraphListData();
         }
-        numEdge = 0;
     }
-
-    public int n() { return Mark.length; } // # of vertices
-    public int e() { return numEdge; }     // # of edges
 
     /** @return v’s first neighbor */
     public int first(int v) {
@@ -88,78 +81,5 @@ public class GraphList implements IGraph {
             return vertex[i].getValue().weight();
         }
         return 0;
-    }
-    /** Set/Get the mark value for a vertex */
-    public void setMark(int v, int val) { Mark[v] = val; }
-    public int getMark(int v) { return Mark[v]; }
-    public void resetAllMarks() {
-        for( int v = 0; v < n(); v++ ) {
-            setMark(v, VisitState.UNVISITED.ordinal());
-        }
-    }
-
-
-    @Override
-    public int compareTo(Object other) throws ClassCastException {
-        if( !(other instanceof IGraph) ) {
-            throw new ClassCastException("An IGraph object expected.");
-        }
-        final IGraph otherG = (IGraph) other;
-        if( this.Mark.length != otherG.n() ) {
-            return this.Mark.length < otherG.n() ? -1 : 1;
-        }
-        if( this.numEdge != otherG.e() ) {
-            return numEdge != otherG.e() ? -1 : 1;
-        }
-
-        this.resetAllMarks();
-
-        for (int v = 0; v < vertex.length; v++) {
-            if( getMark(v) == VisitState.UNVISITED.ordinal() ) {
-                this.setMark(v, VisitState.VISITED.ordinal());
-                for( int w = this.first(v); w < this.n(); w = this.next(v, w)) {
-                    if( this.getMark(w) == VisitState.UNVISITED.ordinal() ) {
-                        if( this.weight(v, w) < otherG.weight(v,w) ) {
-                            return -1;
-                        } else if( this.weight(v, w) > otherG.weight(v,w) ) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        return 0;
-    }
-
-    @Override
-    public boolean equals(Object other) throws ClassCastException {
-        if( !(other instanceof IGraph) ) {
-            throw new ClassCastException("An IGraph object expected.");
-        }
-        final IGraph otherG = (IGraph) other;
-        if( this.Mark.length != otherG.n() ) {
-            return false;
-        }
-        if( this.numEdge != otherG.e() ) {
-            return false;
-        }
-
-        this.resetAllMarks();
-
-        for (int v = 0; v < vertex.length; v++) {
-            if( getMark(v) == VisitState.UNVISITED.ordinal() ) {
-                this.setMark(v, VisitState.VISITED.ordinal());
-                for( int w = this.first(v); w < this.n(); w = this.next(v, w)) {
-                    if( this.getMark(w) == VisitState.UNVISITED.ordinal() ) {
-                        if( this.weight(v, w) != otherG.weight(v,w) ) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-
-        return true;
     }
 }
