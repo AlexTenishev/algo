b8e5b64a80a40eac63fbc7e6bc368e636d300c3c
Alexander Tenishev <atenishev@imvu.com>
2022-08-19 19:14:12 -0700

    add graph path algo impl

--- src/com/altenlab/algo/graph/CoordinatesToGraphHelper.java
+++ src/com/altenlab/algo/graph/CoordinatesToGraphHelper.java
@@ -1,10 +1,17 @@
 package com.altenlab.algo.graph;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.Callable;
 
 /**
  * Inspired by: https://www.geeksforgeeks.org/find-whether-path-two-cells-matrix/
  */
+// FIXME: rename it to IGraphSafeCheck and specialize it either for matrix or adjacency list
 interface  IMatrixSafeCheck {
     boolean isAllowedToOccupy(int[][] space, int x, int y);
 }
@@ -14,10 +21,10 @@ interface  IMatrixSafeCheck {
 //     *     A value of cell 1 means Source.
 //     *     A value of cell 2 means Destination.
 //     *     A value of cell 3 means Blank cell.
-//     *     A value of cell 0 means Blank Wall.
+//     *     A value of cell 4 means Blank Wall.
 //     */
 //    public boolean isAllowedToOccupy(int[][] space, int x, int y) {
-//        return space[x][y] != 0;
+//        return space[x][y] != 4;
 //    }
 //}
 public class CoordinatesToGraphHelper {
@@ -60,17 +67,235 @@ public class CoordinatesToGraphHelper {
                         }
                     }
                 }
-
-//                // source index
-//                if (M[i][j] == 1)
-//                    s = k;
-//
-//                // destination index
-//                if (M[i][j] == 2)
-//                    d = k;
                 ++k;
             }
         }
         return graph;
+    }
+
+    public static class SimpleGraph {
+        final int verticesCount;
+        final List<List<Integer>> adjacencyList;
+
+        protected int start = -1;
+        protected int dest = -1;
+
+        protected final int N;
+        protected final int M;
+
+        public SimpleGraph(final int[][] matrix) {
+            this.N = matrix.length;
+            this.M = matrix[0].length;
+            this.verticesCount = N * M;
+            this.adjacencyList = new ArrayList<>(verticesCount);
+            for( int i = 0; i < verticesCount; ++i ) {
+                this.adjacencyList.add(new ArrayList<>());
+            }
+            buildGraph(matrix);
+        }
+
+        protected void buildGraph(final int[][] matrix) {
+            // fill in graph with N*M vertices
+            int k = 0; // sequence number of current vertex
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (matrix[i][j] != 4) {
+
+                        // connect all 4 adjacent cells to current cell
+                        if (isInBound(i, j + 1, N, M)) {
+                            if (matrix[i][j + 1] != 4) {
+                                addEdge(k, k + 1);
+                            }
+                        }
+                        if (isInBound(i, j - 1, N, M)) {
+                            if (matrix[i][j - 1] != 4) {
+                                addEdge(k, k - 1);
+                            }
+                        }
+                        if (isInBound(i + 1, j, N, M)) {
+                            if (matrix[i + 1][j] != 4) {
+                                addEdge(k, k + M);
+                            }
+                        }
+                        if (isInBound(i - 1, j, N, M)) {
+                            if (matrix[i - 1][j] != 4) {
+                                addEdge(k, k - M);
+                            }
+                        }
+                    }
+
+                    // source index
+                    if (matrix[i][j] == 1)
+                        this.start = k;
+
+                    // destination index
+                    if (matrix[i][j] == 2)
+                        this.dest = k;
+                    ++k;
+                }
+            }
+        }
+
+        // add edge to graph
+        protected void addEdge(int s, int d)
+        {
+            this.adjacencyList.get(s).add(d);
+        }
+
+        // check if there is a path from start vertex to dest vertex
+        public boolean isPathExists()
+        {
+            // Base case
+            if (start == dest) {
+                return true;
+            }
+
+            // Mark all the vertices as not visited
+            boolean[] visited = new boolean[this.verticesCount];
+
+            // Create a queue for BFS
+            final Queue<Integer> queue = new LinkedList<>();
+
+            // Mark the start vertex as visited and enqueue it
+            visited[start] = true;
+            queue.offer(start);
+
+            while( !queue.isEmpty() ) {
+                // Dequeue a vertex from queue
+                final int currentVertex = queue.poll();
+                // this list will be used to get all adjacent vertices of a current vertex
+                final List<Integer> edges = this.adjacencyList.get(currentVertex);
+
+                // Get all adjacent vertices of the
+                // dequeued vertex. If an adjacent vertex has
+                // not been visited, then mark it visited
+                // and enqueue it
+                for( int vertexTo : edges ) {
+                    // If this adjacent node is the
+                    // destination node, then return true
+                    if( vertexTo == dest ) {
+                        return true;
+                    }
+
+                    // Else, continue to do breath first search
+                    if( !visited[vertexTo] ) {
+                        visited[vertexTo] = true;
+                        queue.offer(vertexTo);
+                    }
+                }
+            }
+
+            // if BFS is complete without visiting dest vertex
+            return false;
+        }
+
+        // return first found shortest path from start vertex to dest vertex if it exists
+        protected List<Integer> findPath()
+        {
+            // Base case
+            if (start == dest) {
+                return null;
+            }
+
+            // Mark all the vertices as not visited
+            boolean[] visited = new boolean[this.verticesCount];
+
+            // Create a queue for BFS
+            final Queue<List<Integer>> queue = new LinkedList<>();
+
+            // Mark the start vertex as visited and enqueue it
+            visited[start] = true;
+            List<Integer> currentPath = new LinkedList<>();
+            currentPath.add(start);
+            queue.offer(currentPath);
+
+            while( !queue.isEmpty() ) {
+                // Dequeue a vertex from queue
+                currentPath = queue.poll();
+                final int currentVertex = currentPath.get(currentPath.size() - 1);
+                // this list will be used to get all adjacent vertices of a current vertex
+                final List<Integer> edges = this.adjacencyList.get(currentVertex);
+
+                // Get all adjacent vertices of the
+                // dequeued vertex. If an adjacent vertex has
+                // not been visited, then mark it visited
+                // and enqueue it
+                for( int vertexTo : edges ) {
+                    // If this adjacent node is the
+                    // destination node, then return true
+                    if( vertexTo == dest ) {
+                        currentPath.add(vertexTo);
+                        return currentPath;
+                    }
+
+                    // Else, continue to do breath first search
+                    if( !visited[vertexTo] ) {
+                        visited[vertexTo] = true;
+                        LinkedList<Integer> newPath =  new LinkedList<>(currentPath);
+                        newPath.add(vertexTo);
+                        queue.offer(newPath);
+                    }
+                }
+            }
+
+            // if BFS is complete without visiting dest vertex
+            return null;
+        }
+
+        // return the first found shortest path from start vertex to dest vertex if such path exists
+        // returns null otherwise
+        public List<Pair<Integer, Integer>> getPath() {
+            List<Integer> path = findPath();
+            if( path == null || path.isEmpty() ) {
+                return null;
+            }
+            List<Pair<Integer, Integer>> result = new ArrayList<>(path.size());
+            // now translate path to coordinates
+            for( final Integer k: path ) {
+                final int i = k / M;
+                final int j = M - i * M;
+                result.add(new Pair<>(i, j));
+            }
+            return result;
+        }
+    }
+
+    // Returns true if there is a path from
+    // a source (a cell with value 1)
+    // to a destination (a cell with value 2)
+    //     *     A value of cell 1 means Source.
+    //     *     A value of cell 2 means Destination.
+    //     *     A value of cell 3 means Blank cell.
+    //     *     A value of cell 4 means Blank Wall.
+    public static boolean isPathExist(int[][] matrix)
+    {
+        // Source and destination
+        final SimpleGraph graph = new SimpleGraph(matrix);
+        // corner case
+        if( graph.start == -1 || graph.dest == -1 ) {
+            return false;
+        }
+
+        // find path Using BFS
+        return graph.isPathExists();
+    }
+
+    // Returns path from source (a cell with value 1)
+    // to a destination (a cell with value 2)
+    //     *     A value of cell 1 means Source.
+    //     *     A value of cell 2 means Destination.
+    //     *     A value of cell 3 means Blank cell.
+    //     *     A value of cell 4 means Blank Wall.
+    public static List<Pair<Integer, Integer>> getPath(int[][] matrix)
+    {
+        // Source and destination
+        final SimpleGraph graph = new SimpleGraph(matrix);
+        // corner case
+        if( graph.start == -1 || graph.dest == -1 ) {
+            return null;
+        }
+
+        // find path Using BFS
+        return graph.getPath();
     }
 }
